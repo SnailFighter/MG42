da5ea990f7626962a2bd1a2a02c42160ebe7938d
“snail” <cht_feng@163.com>
2018-09-12 23:36:34 +0800

    modify file

--- test/tets.py
+++ test/tets.py
@@ -1,9 +1,12 @@
 import requests
+import logging
 
 
 print('hello world')
 
 resp = requests.get("https://pub.alimama.com/")
+
+logging.info("test -----------------------")
 
 
 print(requests.get('https://wx.qq.com/'))
